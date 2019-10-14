8b1556f462436fe1fe1c38e3201c6da8603285e9
Alexander Jackson <alexanderjackson@protonmail.com>
2019-10-14 17:03:08 +0100

    Allow files to be parsed into structs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Rule<'a> {
+    non_terminal: &'a str,
+    derivations: Vec<Production<'a>>,
+}
+
+#[derive(Debug)]
+struct Production<'a> {
+    output: Vec<&'a str>,
+}
+
+fn create_production(productions: &str) -> Production {
+    Production {
+        output: productions.split(" ")
+            .collect()
+    }
+}
+
+fn line_to_rule(line: &str) -> Rule {
+    let split: Vec<&str> = line.split(" ::= ")
+        .collect();
+
+    let prods: Vec<Production> = split[1].split(" | ")
+        .map(|x| create_production(x))
+        .collect();
+
+    let r = Rule {
+        non_terminal: split[0],
+        derivations: prods,
+    };
+
+    dbg!(&r);
+
+    r
+}
+
 fn main() {
     let input_file: &str = "grammar.cfg";
     let contents = fs::read_to_string(input_file)
@@ -7,7 +43,12 @@ fn main() {
 
     let lines: Vec<String> = contents.split("\n")
         .map(|l| l.replace("\"", "'"))
+        .filter(|x| x != "")
         .collect();
 
-    dbg!(&lines);
+    let rules: Vec<Rule> = lines.iter()
+        .map(|x| line_to_rule(x))
+        .collect();
+
+    dbg!(&rules);
 }
